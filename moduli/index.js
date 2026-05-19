9b06b204671ffa4ab30ebf31d93cca24ba24a10d
HazeV98 <mattia.bacci.mb@gmail.com>
2026-05-19 17:57:11 +0200

    Add files via upload

--- moduli/index.js
+++ moduli/index.js
@@ -1,4 +1,3 @@
-```javascript
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
 import { getAuth, onAuthStateChanged, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
 import { getFirestore, doc, setDoc, getDoc, collection, getDocs, query, where, orderBy } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
@@ -18,9 +17,6 @@ import { avviaMotoreBachecaTurni } from './bacheca_turni.js';
 import { avviaMotoreBarcadvisor } from './barcadvisor.js';
 import { avviaMotoreBuoniPasto } from './buoni_pasto.js';
 
-// --- IMPORTAZIONI TEMPLATE UI (HTML INIETTATO) ---
-import { initUIBuoniPasto } from './ui_buoniPasto.js';
-
 const firebaseConfig = { 
     apiKey: "AIzaSyDpamGt2bsT6TJMwnerIUTSfCVFBTJtos4", 
     authDomain: "utility-haze.firebaseapp.com", 
@@ -34,9 +30,6 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app); 
 const db = getFirestore(app); 
 const provider = new GoogleAuthProvider();
-
-// Inizializziamo le UI estratte dall'index
-initUIBuoniPasto();
 
 // Inizializziamo il sottomodulo di Autenticazione e Profilo
 avviaMotoreAuth(auth, db, provider);
@@ -1008,5 +1001,3 @@ if (!isStandalone) {
 } else {
     if(installBtn) installBtn.style.display = 'none';
 }
-
-```
