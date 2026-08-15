25d8a965a61de30c887f3bedc0a9eb9155f52d91
HazeV98 <mattia.bacci.mb@gmail.com>
2026-08-15 18:59:02 +0200

    Update modulo function call to include db and auth
    Pass db and auth parameters to the modulo function.

--- moduli/index.js
+++ moduli/index.js
@@ -356,7 +356,8 @@ window.avviaMotoreContattiDaIndex = async () => {
         }
     }
     const modulo = await ModuliLazyLoader.avviaMotore('contatti');
-    if (modulo) modulo();
+    if (modulo) modulo(db, auth); // PASSIAMO DB E AUTH AL MODULO
+    
     const oggiStr = new Date().toISOString().split('T')[0];
     if (window.currentUserData && (window.currentUserData.contatti_access !== true || window.currentUserData.last_contatti_access !== oggiStr)) {
         setDoc(doc(db, "utenti", auth.currentUser.uid), { contatti_access: true, last_contatti_access: oggiStr }, { merge: true });
