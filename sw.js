5bad320c057ca0a91ca539ce8f7e4b328fdd0dcd
HazeV98 <mattia.bacci.mb@gmail.com>
2026-08-23 16:58:39 +0200

    Update service worker cache version to v11

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
-const CACHE_NAME = 'utility-app-v10';
+const CACHE_NAME = 'utility-app-v11';
 
 self.addEventListener('install', (event) => {
-    console.log('[Service Worker] Installato v10');
+    console.log('[Service Worker] Installato v11');
     // Forza l'attivazione immediata del service worker
     self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-    console.log('[Service Worker] Attivato v10');
+    console.log('[Service Worker] Attivato v11');
     return self.clients.claim();
 });
 
